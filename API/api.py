0dc0359eaf29696030d23392cadbbd15dc5cb767
Brett Stine <r.brettstine@gmail.com>
2024-04-28 22:45:18 -0400

    Initial commit - flask app

---
+++ API/api.py
@@ -0,0 +1,54 @@
+import os
+import re
+import sys
+import glob
+import pandas as pd
+import numpy as np
+import json
+from flask import Flask, render_template, Blueprint, request, send_from_directory, send_file, flash, Blueprint, g, session, app, current_app
+from flask_wtf import Form
+from BLL.bll import BusinessLogicLayer
+from bs4 import BeautifulSoup as bs
+from werkzeug.utils import secure_filename
+from pathlib import Path
+
+
+this_files_dir = os.path.dirname(os.path.abspath(__file__))
+
+webapp = Blueprint("webapp",
+          __name__,
+          template_folder=f"{this_files_dir}/../DEPENDENCIES/HTML",
+          static_folder="DEPENDENCIES/JS",
+          static_url_path="/static"
+          )
+
+bll = BusinessLogicLayer()
+
+def get_js__and_css_source():
+
+  # Add js files here as you create them
+
+  js_dir = f"{this_files_dir}/../DEPENDENCIES/JS"
+  css_dir = f"{this_files_dir}/../DEPENDENCIES/CSS"
+
+  js_source = ""
+  for js_filename in glob.glob(f"{js_dir}/*.js"):
+    with open(js_filename, "r") as f:
+      js_source += f.read()
+
+  css_source = ""
+  for css_filename in glob.glob(f"{css_dir}/*.css"):
+    with open(css_filename, "r") as f:
+      css_source += f.read()
+
+  return js_source, css_source
+
+@webapp.route("/")
+def go():
+  js_source, css_source = get_js__and_css_source()
+
+  return render_template("index.html.j2",
+              js_source=js_source,
+              css_source=css_source,
+              foo="<p>foo</p>"
+              )
---
+++ web-server.py
@@ -0,0 +1,28 @@
+import setproctitle
+from gevent.pywsgi import WSGIServer
+from API.api import webapp # where the magic happens
+from flask import Flask, redirect, url_for, render_template, Blueprint, session, app, flash
+import os
+
+debug = True
+
+hostname = 'Brett'
+
+app = Flask(__name__)
+app.register_blueprint(webapp)
+
+# app.config['UPLOAD_FOLDER'] = '/static/'
+# app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
+
+def main():
+
+	#For developers, use this
+	app.run(debug=debug, host='127.0.0.1', port=5001)
+
+	# For server, use this
+	# app.config['SERVER_NAME'] = 'facemorph.com:5000'	# only for running on server
+	# app.run(debug=debug)
+
+	# Magically calling route "/" in api_routes.py (O.o)
+
+main()
