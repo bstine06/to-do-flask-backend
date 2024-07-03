4b398b41d0b2ae2d075aafbb6905e03391201a75
Brett Stine <r.brettstine@gmail.com>
2024-07-03 15:09:16 -0400

    Add flask_cors and configure to allow api requests
      - only allow api requests from specific origin (front end application)

--- web-server.py
+++ web-server.py
@@ -3,13 +3,17 @@ from gevent.pywsgi import WSGIServer
 from API.api import webapp # where the magic happens
 from flask import Flask, redirect, url_for, render_template, Blueprint, session, app, flash, jsonify
 import os
+from flask_cors import CORS
 
 debug = True
 
 hostname = 'Brett'
 
+ALLOWED_ORIGIN = "http://127.0.0.1:5500" # the only external domain allowed to request this server
+
 app = Flask(__name__)
 app.register_blueprint(webapp)
+CORS(app, origins=ALLOWED_ORIGIN)  # Allow only this origin for all routes
 
 # app.config['UPLOAD_FOLDER'] = '/static/'
 # app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
